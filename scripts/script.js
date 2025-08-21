d9c331525ab7aff6d7a2c0265c116695e76b761d
Radosław WIeczorek <r.p.wieczorek@gmail.com>
2025-08-21 09:12:54 +0200

    Fix script

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,19 @@
-const accTab = document.querySelectorAll('#es-rc #es-rc-content .acc-tab');
+const baseWrapperSelector = '#es-rc #es-rc-content';
+const accTab = document.querySelectorAll(baseWrapperSelector + ' .acc-tab');
 
 for (let i = 0; i < accTab.length; i++) {
   accTab[i].addEventListener('click', function () {
-    this.classList.add('active');
+    this.classList.add('es-active');
     let accTabContent = this.nextElementSibling;
 
     const hideContent = () => {
       accTabContent.style.maxHeight = null;
-      this.classList.remove('active');
+      this.classList.remove('es-active');
     };
 
     const showContent = () => {
       accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
-      this.classList.add('active');
+      this.classList.add('es-active');
     };
 
     if (accTabContent.style.maxHeight) {
@@ -23,8 +24,6 @@ for (let i = 0; i < accTab.length; i++) {
   });
 }
 
-const baseWrapperSelector = '#es-rc #es-rc-content';
-
 const swiper1Wrapper = baseWrapperSelector + ' .es-cross-mobile';
 const swiper1Selector = swiper1Wrapper + ' .es-swiper1';
 const swiper1 = new Swiper(swiper1Selector, {
@@ -32,12 +31,12 @@ const swiper1 = new Swiper(swiper1Selector, {
   loop: true,
 
   pagination: {
-    el: '.es-swiper1-pagination',
+    el: baseWrapperSelector + ' .es-swiper1-pagination',
   },
 
   navigation: {
-    nextEl: '.es-swiper1-button-next',
-    prevEl: '.es-swiper1-button-prev',
+    nextEl: baseWrapperSelector + ' .es-swiper1-button-next',
+    prevEl: baseWrapperSelector + ' .es-swiper1-button-prev',
   },
 
 });
@@ -49,11 +48,11 @@ const swiper2 = new Swiper(swiper2Selector, {
   loop: true,
 
   pagination: {
-    el: '.es-swiper2-pagination',
+    el: baseWrapperSelector + ' .es-swiper2-pagination',
   },
 
   navigation: {
-    nextEl: '.es-swiper2-button-next',
-    prevEl: '.es-swiper2-button-prev',
+    nextEl: baseWrapperSelector + ' .es-swiper2-button-next',
+    prevEl: baseWrapperSelector + ' .es-swiper2-button-prev',
   },
 });
